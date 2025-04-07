c499d2cd2fac6680b4994f4e929bba64717a8534
AkashSingh1141144 <AkashSingh.Webdeveloper@gmail.com>
2025-04-07 11:19:11 +0530

    My first NextJs App

---
+++ my-first-nextjs-app/src/app/akash.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Akash = () => {
+    return (
+		<>
+			<h1 className="text-3xl text-green-600 font-bold underline text-center justify-center items-center mt-36">Akash Kumar Singh</h1>
+		</>
+	)
+}
+
+export default Akash;
---
+++ my-first-nextjs-app/src/app/page.tsx
@@ -0,0 +1,12 @@
+// import Image from "next/image";
+import Akash from "./akash";
+
+export default function Home() {
+  return (
+    <>
+        <Akash />
+
+        <p className="text-2xl font-bold text-center mt-10">My life Partner Name is <span className=" text-blue-500 font-extrabold text-9xl mt-2 underline">Sandhya Singh</span></p>
+    </>
+  );
+}
