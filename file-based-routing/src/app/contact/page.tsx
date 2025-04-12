6983fb0630d9e479b056714558f842c6fbd29943
AkashSingh1141144 <AkashSingh.Webdeveloper@gmail.com>
2025-04-12 09:39:50 +0530

    File-Structure Of Next Js

---
+++ file-based-routing/src/app/contact/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function Contact() {
+	return (
+		<>
+			<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+				  <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+					<Image
+					  className="dark:invert"
+					  src="/next.svg"
+					  alt="Next.js logo"
+					  width={180}
+					  height={38}
+					  priority
+					/>
+					<h1 className="text-9xl font-bold text-center">Pallavi | Akash Singh</h1>
+					<p className="text-2xl text-center bg-amber-700 p-2">Contact Page</p>
+				  </main>
+				</div>
+		</>
+	)
+}
